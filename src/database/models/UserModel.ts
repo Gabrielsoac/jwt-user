d68d84fe4d787a7990389068b192e79bc969e021
Gabriel Soares <gabrielsoacc@gmail.com>
2025-02-23 15:29:25 -0300

    feat: created User Model

---
+++ src/database/models/UserModel.ts
@@ -0,0 +1,27 @@
+import { model, Schema } from "mongoose";
+
+type TUserSchema = {
+    name: string,
+    email: string,
+    password: string
+
+
+}
+
+const userSchema = new Schema<TUserSchema>(
+    {
+        name: {type: String, required: true},
+        email: {type: String, required: true},
+        password: {type: String, required: true} 
+    }
+);
+
+userSchema.index(
+    {email: 1},
+    {unique: true}
+)
+
+export const UserModel = model<TUserSchema>(
+    'User',
+    userSchema
+)
