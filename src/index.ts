5db262c869834e6f4706d09355be3b5da3893fa8
Gabriel Soares <gabrielsoacc@gmail.com>
2025-02-23 12:57:11 -0300

    feat: added web server with express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("hello my web server");
+import { app } from './server/Server'
+import 'dotenv/config';
+
+const port = process.env.PORT || 4000;
+
+function runServer () {
+    console.log('hello, my web server');
+    console.log(`Running server on port ${port}`);
+}
+
+app.listen(
+    port,
+    runServer
+)
